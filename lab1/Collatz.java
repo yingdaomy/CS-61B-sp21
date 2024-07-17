d2ce70c66c688e025cde0a04daf3d379d049be0c
YingDaomy <yingdaomy37@gmail.com>
2024-07-18 01:54:59 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -5,13 +5,14 @@ public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
-        }
+        /* @pram n is the current number in the sequence
+         *  @return the next number in the sequence
+         */
+            if (n % 2 == 0) {
+                return n / 2;
+            }else {
+                return 3 * n + 1;
+            }
     }
 
     public static void main(String[] args) {
